8bd9a1187ae510ff42b384949a105c48219dc3d2
mircothibes <mircothibes@gmail.com>
2025-10-22 19:08:50 +0200

    docs: add app/main.py

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="fast-crud")
+
+@app.get("/")
+def read_root():
+    return {"message": "fast-crud API is running"}
+
+
+@app.get("/health"):
+def health():
+    return {"stuts": "ok"}
