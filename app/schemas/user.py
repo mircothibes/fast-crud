c45a1a8694dff10c84bebb17db9e933fefbd533e
mircothibes <mircothibes@gmail.com>
2025-10-27 19:27:06 +0100

    docs: update app/schemas/user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 class UserBase(BaseModel):
-    name: str
+    name: str = Field(min_length=3, max_length=100)
     email: EmailStr
 
 class UserCreate(UserBase):
